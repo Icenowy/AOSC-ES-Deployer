4e93b5434a74878bccfef10fafbd85beba72e1d4
Icenowy Zheng <icenowy@outlook.com>
2015-12-05 01:03:34 +0800

    doc: add cut package qt-5-doc

--- deployer/slices/doc.sh
+++ deployer/slices/doc.sh
@@ -1,7 +1,7 @@
 slice_init doc
 
 # FIXME: maxima, guile depends on texinfo
-slice_doc_cut_packages="gnome-user-docs man-db"
+slice_doc_cut_packages="gnome-user-docs man-db qt-5-doc "
 
 slice_doc_cutdown_process() {
 	echo "Purging documents"
