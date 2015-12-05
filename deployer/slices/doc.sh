75d76a1aa87d6c906ed980e7588da9403b13dfe5
Icenowy Zheng <icenowy@outlook.com>
2015-12-05 10:55:28 +0800

    package: poor implementation. slices.doc: fixed wrong ""

--- deployer/slices/doc.sh
+++ deployer/slices/doc.sh
@@ -6,5 +6,4 @@ slice_doc_cut_packages="gnome-user-docs man-db qt-5-doc "
 slice_doc_cutdown_process() {
 	echo "Purging documents"
 	sudo rm -rf $RMFLAGS $TARGET/usr/{,share/}{info,man,doc} 
-	echo "Purging 
 }
