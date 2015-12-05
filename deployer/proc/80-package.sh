688a1fb8204c62895decfa9e9c532d03d7843645
Icenowy Zheng <icenowy@outlook.com>
2015-12-05 12:04:27 +0800

    package: work

--- deployer/proc/80-package.sh
+++ deployer/proc/80-package.sh
@@ -1,4 +1,4 @@
 if [ "$CONFIG_PACK_TARBALL" = "y" ]; then
 	export TARBALL_CANNONICAL_PATH="$(readlink -f $CONFIG_PACK_TARBALL_PATH)"
-	( cd target; tar cvfJ "$TARBALL_CANNONICAL_PATH" * )
+	( cd target; sudo tar cvfJ "$TARBALL_CANNONICAL_PATH" * )
 fi
